169721c650dc81b3b031a200cac388ab48f01e9e
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2016-09-04 07:17:16 +0800

    加入mongoose

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const port = process.env.PORT || 5000;
+const mongodbUri = process.env.MONGODB_URI;
 
-
+app.use(bodyParser.urlencoded({ extended: true }))
+mongoose.connect(mongodbUri);
 app.use(require('./controllers'));
 app.listen(port, () => { console.log('now listening on port ' + port); })
