7075f3a81d7a5e1c62c3bc9bcfcfd08d90d8fc71
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2016-09-05 00:14:45 +0800

    利用google chart繪圖

--- public/js/index.js
+++ public/js/index.js
@@ -1,37 +1,22 @@
-var lineData = {
-    labels: ['Data 1', 'Data 2', 'Data 3', 'Data 4',
-        'Data 5', 'Data 6', 'Data 7'],
-    datasets: [{
-        fillColor: 'rgba(0,0,0,0)',
-        strokeColor: 'rgba(220,180,0,1)',
-        pointColor: 'rgba(220,180,0,1)',
-        data: [20, 30, 80, 20, 40, 10, 60]
-    }, {
-            fillColor: 'rgba(0,0,0,0)',
-            strokeColor: 'rgba(151,187,205,1)',
-            pointColor: 'rgba(151,187,205,1)',
-            data: [60, 10, 40, 30, 80, 30, 20]
-        }]
+google.charts.load('current', { 'packages': ['corechart'] });
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
+    var data = google.visualization.arrayToDataTable([
+        ['Year', 'Sales', 'Expenses'],
+        ['2004', 1000, 400],
+        ['2005', 1170, 460],
+        ['2006', 660, 1120],
+        ['2007', 1030, 540]
+    ]);
+
+    var options = {
+        title: 'Company Performance',
+        curveType: 'function',
+        legend: { position: 'bottom' }
+    };
+
+    var chart = new google.visualization.LineChart(document.getElementById('tempChart'));
+
+    chart.draw(data, options);
 }
-
-Chart.defaults.global = {
-    animationSteps: 50,
-    tooltipYPadding: 16,
-    tooltipCornerRadius: 0,
-    tooltipTitleFontStyle: 'normal',
-    tooltipFillColor: 'rgba(0,160,0,0.8)',
-    animationEasing: 'easeOutBounce',
-    scaleLineColor: 'black',
-    scaleFontSize: 16
-}
-
-var ctx = document.getElementById('tempChart').getContext('2d');
-var lineDemo = new Chart(ctx).Line(lineData, {
-    responsive: true,
-    pointDotRadius: 10,
-    bezierCurve: false,
-    scaleShowVerticalLines: false,
-    scaleGridLineColor: 'black'
-});
-
-Chart.defaults.global.responsive = true;
