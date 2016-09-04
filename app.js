8110369643ae3434c4765c648b8672f551e121cc
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2016-09-04 20:28:40 +0800

    重構

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ const port = process.env.PORT || 5000;
 const mongodbUri = process.env.MONGODB_URI;
 
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+})
 mongoose.connect(mongodbUri);
 app.use(require('./controllers'));
 app.listen(port, () => { console.log('now listening on port ' + port); })
--- controllers/index.js
+++ controllers/index.js
@@ -3,16 +3,7 @@ const router = express.Router();
 const bodyParser = require('body-parser');
 const accessDB = require('../models/access-db.js');
 
-router.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    next();
-})
-
-router.get('/', (req, res) => {
-    res.send('you cannot access this page.');
-})
-
+router.get('/', (req, res) => { res.send('you cannot access this page.'); })
 router.get('/insertTH', accessDB.insertTH);
 router.get('/selectTH', accessDB.selectTH);
 
