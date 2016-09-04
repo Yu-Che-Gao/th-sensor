ea0235409f2d92327d61520520765145dd9d8e32
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2016-09-04 22:32:36 +0800

    重新排版

--- controllers/index.js
+++ controllers/index.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const accessDB = require('../models/access-db.js');
 
 router.get('/', (req, res) => { res.send('you cannot access this page.'); })
-router.get('/dashboard', (req, res) => { res.render('index', { title: 'Hey', content: 'Hello there!' }) });
+router.get('/dashboard', (req, res) => { res.render('index', { title: 'TH-Sensor'}) });
 router.post('/insertTH', accessDB.insertTH);
 router.post('/selectTH', accessDB.selectTH);
 
