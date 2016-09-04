c987ed79ce66be0a95c453e574d76bc01ac27503
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2016-09-04 21:15:20 +0800

    將接口改為POST

--- controllers/index.js
+++ controllers/index.js
@@ -5,7 +5,7 @@ const accessDB = require('../models/access-db.js');
 
 router.get('/', (req, res) => { res.send('you cannot access this page.'); })
 router.get('/dashboard', (req, res) => { res.render('index', { title: 'Hey', content: 'Hello there!' }) });
-router.get('/insertTH', accessDB.insertTH);
-router.get('/selectTH', accessDB.selectTH);
+router.post('/insertTH', accessDB.insertTH);
+router.post('/selectTH', accessDB.selectTH);
 
 module.exports = router;
--- models/access-db.js
+++ models/access-db.js
@@ -1,8 +1,8 @@
 const thDbs = require('./th-dbs.js');
 function insertTH(req, res) {
     let data = new thDbs({
-        temp: req.query.temp,
-        humi: req.query.humi,
+        temp: req.body.temp,
+        humi: req.body.humi,
     });
 
     data.save().then((result) => {
@@ -13,20 +13,19 @@ function insertTH(req, res) {
 }
 
 function selectTH(req, res) {
-    if (typeof (req.query.lowerTime) == 'undefined' || typeof (req.query.lowerTime) == 'undefined') {
+    if (typeof (req.body.lowerTime) == 'undefined' || typeof (req.body.lowerTime) == 'undefined') {
         thDbs.find({}).exec().then((result) => {
             res.send(JSON.stringify(result));
         }).catch((err) => {
             res.send('error: ' + err);
         })
     } else {
-        thDbs.find({ insertTime: { "$in": [req.query.lowerTime, req.query.upperTime] } }).exec().then((result) => {
+        thDbs.find({ insertTime: { "$in": [req.body.lowerTime, req.body.upperTime] } }).exec().then((result) => {
             res.send(JSON.stringify(result));
         }).catch((err) => {
             res.send('error: ' + err);
         })
     }
-
 }
 
 exports.insertTH = insertTH;
