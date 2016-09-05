ec9e920268e71bfcc1c6d51a186decfeab279d06
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2016-09-05 11:44:36 +0800

    微調

--- public/js/index.js
+++ public/js/index.js
@@ -3,20 +3,19 @@ google.charts.setOnLoadCallback(drawChart);
 
 function drawChart() {
     var data = google.visualization.arrayToDataTable([
-        ['Year', 'Sales', 'Expenses'],
-        ['2004', 1000, 400],
-        ['2005', 1170, 460],
-        ['2006', 660, 1120],
-        ['2007', 1030, 540]
+        ['Time', 'Temperature'],
+        ['2004', 1000],
+        ['2005', 1170],
+        ['2006', 660],
+        ['2007', 1030]
     ]);
 
     var options = {
-        title: 'Company Performance',
+        title: '歷史溫度曲線圖',
         curveType: 'function',
         legend: { position: 'bottom' }
     };
 
     var chart = new google.visualization.LineChart(document.getElementById('tempChart'));
-
     chart.draw(data, options);
 }
